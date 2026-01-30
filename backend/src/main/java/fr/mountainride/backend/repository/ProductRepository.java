b2718effd6c01820980502de9db438f97e7c6aab
Lucas <lucas.maiaux@le-campus-numerique.fr>
2026-01-30 10:49:13 +0100

    add repository for Product

---
+++ backend/src/main/java/fr/mountainride/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package fr.mountainride.backend.repository;
+
+import fr.mountainride.backend.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
